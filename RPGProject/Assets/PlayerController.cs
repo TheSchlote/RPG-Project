0d50751b336d12fe268363d3e3d5abbc7ebe9487
TheSchlote <theschlote@gmail.com>
2019-10-23 19:01:14 -0500

    MOVEMENT - Making an RPG in Unity (E01)

---
+++ RPGProject/Assets/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask movementmask;
+    Camera cam;
+    PlayerMotor motor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100, movementmask))
+            {
+                motor.MoveToPoint(hit.point);
+
+                //Move out plaer to what we hit
+
+                //stop focusing any objects
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                //Check if we hit an interactable
+                //if we did, set it as our focus
+            }
+        }
+    }
+}
